c098e31b23bbe7865a6fbb4c32fe8273684a3cc8
Nilton Lopes <nilton-rck@hotmail.com>
2018-12-03 23:11:27 -0200

    changing help command message

--- src/discord/commands/randon/help.js
+++ src/discord/commands/randon/help.js
@@ -1,20 +1,21 @@
-const commando = require('discord.js-commando');
+const commando = require("discord.js-commando")
 
-class helpCommand extends commando.Command{
-	constructor(client){
-		super(client,{
-			name:'helplist',
-			group: 'random',
-			memberName:'helplist',
-			description:'list of commands',
-			examples:['helplist']
-		});
-	}
+class helpCommand extends commando.Command {
+  constructor(client) {
+    super(client, {
+      name: "helplist",
+      group: "random",
+      memberName: "helplist",
+      description: "list of commands",
+      examples: ["helplist"]
+    })
+  }
 
-	async run(message, args){
-		message.reply("Fraga só. Nessa lista aí tem os bang que sei fazer. Só dar um toque aí que em dois tempo eu faço.\n**Buscar jobs**\n• !jobs\n• !jobsweek\n• !jobsme\n");
-	}
+  async run(message, args) {
+    message.reply(
+      "Até o momento consigo fazer os seguintes comandos:.\n**Buscar jobs**\n• !jobs\n• !jobsweek\n• !jobsme\n"
+    )
+  }
 }
 
-
-module.exports = helpCommand;
+module.exports = helpCommand
