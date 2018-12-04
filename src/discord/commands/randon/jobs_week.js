4d61e713d013c019b7720df94274a0018655240c
Nilton Lopes <nilton-rck@hotmail.com>
2018-12-03 23:17:15 -0200

    testing git stash

--- src/discord/commands/randon/jobs_week.js
+++ src/discord/commands/randon/jobs_week.js
@@ -1,20 +1,21 @@
-const commando = require('discord.js-commando');
+const commando = require("discord.js-commando")
 
-class jobsWeekCommand extends commando.Command{
-	constructor(client){
-		super(client,{
-			name:'jobsweek',
-			group: 'random',
-			memberName:'jobsweek',
-			description:'Check for jobs.',
-			examples:['jobsweek']
-		});
-	}
+class jobsWeekCommand extends commando.Command {
+  constructor(client) {
+    super(client, {
+      name: "tramposdasemana",
+      group: "random",
+      memberName: "tramposdasemana",
+      description: "Check for jobs.",
+      examples: ["tramposdasemana"]
+    })
+  }
 
-	async run(message, args){
-		message.reply("A agência tá maior parada essa semana. Aproveita e fica de boa aí!");
-	}
+  async run(message, args) {
+    message.reply(
+      "A agência tá maior parada essa semana. Aproveita e fica de boa aí!"
+    )
+  }
 }
 
-
-module.exports = jobsWeekCommand;
+module.exports = jobsWeekCommand
